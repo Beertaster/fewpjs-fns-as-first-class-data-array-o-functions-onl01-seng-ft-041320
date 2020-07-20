30680afb8a91b033bc3a43150e7c59c44df5c881
William Stevens <wstevens.hornet@gmail.com>
2020-07-20 20:08:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ let routine = [
   unleashDog
 ]
 
-function excersiseDog(dogName, dogBreed){
+function excerciseDog(dogName, dogBreed){
   let array = []
   for (i = 0 ; i < routine.length; i++) {
     array.push(routine[i](dogName, dogBreed))
